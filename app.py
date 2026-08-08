d76b774cfc6f86b3dd0aed664c5f42aedb81739a
asb <abdulsomadbakare@gmail.com>
2026-08-08 21:27:32 +0100

    Fix static route conflict causing 404 on clean URLs

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ load_dotenv()
 from flask import Flask, send_from_directory
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder='frontend', static_url_path='')
+app = Flask(__name__, static_folder=None)
 CORS(app)
+
+FRONTEND_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'frontend')
 
 from routes.auth import auth_bp
 from routes.menu import menu_bp
@@ -24,18 +26,18 @@ app.register_blueprint(analytics_bp, url_prefix='/api')
 @app.route('/<path:path>')
 def serve_frontend(path):
     if path == '':
-        return send_from_directory(app.static_folder, 'index.html')
+        return send_from_directory(FRONTEND_FOLDER, 'index.html')
 
-    full_path = os.path.join(app.static_folder, path)
+    full_path = os.path.join(FRONTEND_FOLDER, path)
 
     if os.path.isfile(full_path):
-        return send_from_directory(app.static_folder, path)
+        return send_from_directory(FRONTEND_FOLDER, path)
 
     html_path = f"{path}.html"
-    if os.path.isfile(os.path.join(app.static_folder, html_path)):
-        return send_from_directory(app.static_folder, html_path)
+    if os.path.isfile(os.path.join(FRONTEND_FOLDER, html_path)):
+        return send_from_directory(FRONTEND_FOLDER, html_path)
 
-    return send_from_directory(app.static_folder, 'index.html')
+    return send_from_directory(FRONTEND_FOLDER, 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
